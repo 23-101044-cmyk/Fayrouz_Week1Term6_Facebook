1efd21919969bc87e05b07032d455b2406e49096
Group One <groupone@Asma-iMac.local>
2026-02-11 10:57:17 +0200

    first

---
+++ src/pages/facebook.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './facebook.css';
+
+const Facebook = () => {
+    return ( 
+        <body>
+            <h1>hi</h1>
+        </body>
+     );
+}
+ 
+export default Facebook;
